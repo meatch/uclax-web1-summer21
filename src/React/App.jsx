476846e4eb3fc478999e395effc6d9456cd2ff1d
Mitchell Gohman <meatch@me.com>
2021-08-09 19:36:03 -0700

    Adding Hambruger Icon

--- src/React/App.jsx
+++ src/React/App.jsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 import { MediaQueryProvider } from 'common/mediaQueries/useMediaQuery.js';
 
 import Header from './Layout/Header.jsx';
-import Nav from './Layout/Nav.jsx';
+import Navigation from './Layout/Navigation/Navigation.jsx';
 import Main from './Layout/Main.jsx';
 import Footer from './Layout/Footer.jsx';
 
@@ -16,7 +16,7 @@ const App = () => {
             <BrowserRouter>
                 <AppStyled>
                     <Header />
-                    <Nav />
+                    <Navigation />
                     <Main />
                     <Footer />
                 </AppStyled>
